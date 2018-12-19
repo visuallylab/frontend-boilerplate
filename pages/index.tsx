1b1d670b40d9198b823465714b8479d1e8b5fc54
JHIH-CING LI <jhihcing.li@gmail.com>
2018-12-19 17:07:05 +0800

    Sync tslint and tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,29 @@ import Section from '../components/Section';
 
 export default () => (
   <Layout title='About | Next.js + TypeScript Example'>
-  <Section title='About 👀'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eveniet sunt aperiam architecto, eligendi quaerat neque aliquid saepe alias nulla, quasi vitae corrupti quas beatae, voluptatem nobis blanditiis magni voluptate.</p>
+    <Section title='About 👀'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Optio eveniet sunt aperiam architecto,
+        eligendi quaerat neque aliquid saepe alias nulla,
+        quasi vitae corrupti quas beatae, voluptatem nobis blanditiis magni voluptate.
+      </p>
     </Section>
     <Section title='team 👀'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eveniet sunt aperiam architecto, eligendi quaerat neque aliquid saepe alias nulla, quasi vitae corrupti quas beatae, voluptatem nobis blanditiis magni voluptate.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Optio eveniet sunt aperiam architecto,
+        eligendi quaerat neque aliquid saepe alias nulla,
+        quasi vitae corrupti quas beatae, voluptatem nobis blanditiis magni voluptate.
+      </p>
     </Section>
     <Section title='Give it a try 👀'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eveniet sunt aperiam architecto, eligendi quaerat neque aliquid saepe alias nulla, quasi vitae corrupti quas beatae, voluptatem nobis blanditiis magni voluptate.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Optio eveniet sunt aperiam architecto,
+        eligendi quaerat neque aliquid saepe alias nulla,
+        quasi vitae corrupti quas beatae, voluptatem nobis blanditiis magni voluptate.
+      </p>
     </Section>
   </Layout>
 );
