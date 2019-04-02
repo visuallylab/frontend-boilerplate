f0b013b36910a51471a0605a91d945461be2fe33
TseHang <a54383813@gmail.com>
2019-04-02 18:35:34 +0800

    fix path

--- environment.ts
+++ environment.ts
@@ -4,3 +4,4 @@ export const env = (key: string, defaultValue = '') =>
   process.env[key] || defaultValue;
 
 export const GITHUB = env('DEPLOY_ENV') === 'github';
+export const PROJ_NAME = env('PROJ_NAME');
--- utils.ts
+++ utils.ts
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
-import { GITHUB, env } from './environment';
+import { GITHUB, PROJ_NAME } from './environment';
 
 export function getRelativePath(path: string): string {
   if (GITHUB) {
     const valid = /[^\/].*/g.exec(path);
-    return valid ? `/${env('PROJ_NAME')}/${valid[0]}` : '';
+    return valid ? `/${PROJ_NAME}/${valid[0]}` : `/${PROJ_NAME}/`;
   }
   return path;
 }
