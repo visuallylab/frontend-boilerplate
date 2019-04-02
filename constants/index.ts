be1221df974c67f0569e9b96871af8ac7e909aa4
TseHang <a54383813@gmail.com>
2019-04-02 20:01:44 +0800

    fix icon path

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,9 @@
+import { getRelativePath } from '@/utils';
+
 /* head meta tag data */
 export const SITE_TITLE = 'Visually Lab';
 export const SITE_DESC = 'visualize everything';
 export const SITE_URL = 'localhost';
-export const FAVICON_PATH = '/static/favicon.ico';
-export const LARGE_ICON_PATH = '/static/large-icon.png';
+export const FAVICON_PATH = getRelativePath('/static/favicon.ico');
+export const LARGE_ICON_PATH = getRelativePath('/static/large-icon.png');
 export const NAV_TITLE = 'nav title';
